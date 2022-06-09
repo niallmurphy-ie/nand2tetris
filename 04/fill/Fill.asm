928d913bef4c8d75f69834e1f970511dede9bfbb
Niall Murphy <leturf@gmail.com>
2022-06-09 18:35:42 +0700

    Mult and Fill working

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -6,9 +6,60 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+(LOOP)
+		// Load SCREEN into addr
+		@SCREEN
+		D=A
+		@addr
+		M=D // Sets to start
+		// Load blocks into i
+		@8192
+		D=A
+		@i
+		M=D
+		@addr
+		M=M+D // Sets to SCREEN + i
+		// Get current keyboard
+		@KBD
+		D=M
+		@colour
+		M=0
+		// If 0 --> GOTO Print White
+		@PRINT
+		D;JEQ
+		// BLACK
+		@colour
+		M=-1
+		// GOTO Print Black
+		@PRINT
+		D;JEQ
+
+		(PRINT)
+			// Load i and check if 0
+			@i
+			D=M
+			@LOOP
+			D+1;JEQ // Go back to start if back to SCREEN
+
+			// Print
+			@colour
+			D=M
+			// Set colour
+			@addr
+			A=M
+			M=D
+			// Decrement
+			@i
+			M=M-1
+			@addr
+			M=M-1
+			@PRINT
+			0;JEQ
+	@LOOP
+		0;JEQ
--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -10,3 +10,40 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+	@R0
+	D=M
+	@x
+	M=D
+	@R1
+	D=M
+	@y
+	M=D
+	@R2 // SUM
+	M=0
+	@i
+	M=0
+
+	(LOOP)
+		// Compare i to y (max)
+		@i
+		D=M
+		@y
+		D=D-M
+		// Jump to END if D == 0
+		@END
+		D;JEQ
+		// Sum
+		@x
+		D=M
+		@R2
+		M=M+D
+		// Iterate i
+		@i
+		M=M+1
+		// Loop
+		@LOOP
+		0;JEQ
+	(END)
+		@END
+		0;JEQ
+
